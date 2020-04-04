7824c96bd75d2be6d53bbc3161412c956fae2b90
Lucas Hyatt <llhyatt98@gmail.com>
2020-04-03 21:50:10 -0700

    Add arrMax to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -10,3 +10,12 @@ arrAvg = (arr) => {
 
 // var val1 = arrAvg([1, 3, 5]);
 // console.log(val1);
+
+arrMax = (arr) => {
+	var max_val = arr[0];
+	arr.forEach((item, index, array) => { if(item > max_val){max_val = item;} });
+	return max_val;
+}
+
+// var val2 = arrMax([1, 50, 10]);
+// console.log(val2);
