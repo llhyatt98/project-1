d72c33490eeab63e3902430f8ef1973cc4aa8b8c
Lucas Hyatt <llhyatt98@gmail.com>
2020-04-03 21:56:04 -0700

    Add sumEvens to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,5 +1,7 @@
 // jshint esversion: 6 
 
+
+// Finds average of an array
 arrAvg = (arr) => {
 	var total = 0;
 	for (var i = 0; i < arr.length; i++) {
@@ -11,6 +13,10 @@ arrAvg = (arr) => {
 // var val1 = arrAvg([1, 3, 5]);
 // console.log(val1);
 
+
+
+
+// Finds max value in an array
 arrMax = (arr) => {
 	var max_val = arr[0];
 	arr.forEach((item, index, array) => { if(item > max_val){max_val = item;} });
@@ -19,3 +25,18 @@ arrMax = (arr) => {
 
 // var val2 = arrMax([1, 50, 10]);
 // console.log(val2);
+
+
+
+
+//Finds the sum of even numbers in an array
+sumEvens = (arr) => {
+	var total = 0;
+	for(let i of arr){
+		if(i % 2 == 0){ total += i; }
+	}
+	return total;
+}
+
+// var val3 = sumEvens([1, 2, 3, 4, 1]);
+// console.log(val3);
